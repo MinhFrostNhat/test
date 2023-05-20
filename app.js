8147dd36ba75b3428f384b8cb9da3b71012d576c
MinhFrostNhat <100963934+MinhFrostNhat@users.noreply.github.com>
2023-05-21 00:23:20 +0700

    update port for test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const winston = require("winston");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const logger = winston.createLogger({
   level: "info",
